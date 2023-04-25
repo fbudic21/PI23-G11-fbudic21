2f820738c04fc17c9fbf623e165aa20cfd73175c
Petra Juroš <pjuros21@foi.hr>
2023-04-25 17:49:09 +0200

    Doradena forma za LogIn

--- EvaluationManagerG11/frmLogIn.cs
+++ EvaluationManagerG11/frmLogIn.cs
@@ -9,8 +9,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace EvaluationManagerG11 {
-    public partial class Form1 : Form {
-        public Form1() {
+    public partial class frmLogIn : Form {
+        public frmLogIn() {
             InitializeComponent();
         }
     }
