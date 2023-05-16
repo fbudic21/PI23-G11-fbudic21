48fa052bbdf4e7d4cc0b69ecf19e730f8890e38e
fbudic21 <fbudic21@foi.hr>
2023-05-16 17:46:43 +0200

    Log in poboljšan. Dodan datum itd.

--- FrmEvaluation.Designer.cs
+++ FrmEvaluation.Designer.cs
@@ -45,58 +45,66 @@
             // 
             this.cboActivities.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cboActivities.FormattingEnabled = true;
-            this.cboActivities.Location = new System.Drawing.Point(62, 45);
+            this.cboActivities.Location = new System.Drawing.Point(83, 55);
+            this.cboActivities.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cboActivities.Name = "cboActivities";
-            this.cboActivities.Size = new System.Drawing.Size(121, 21);
+            this.cboActivities.Size = new System.Drawing.Size(160, 24);
             this.cboActivities.TabIndex = 0;
             this.cboActivities.SelectedIndexChanged += new System.EventHandler(this.cboActivities_SelectedIndexChanged);
             // 
             // txtMinForSignature
             // 
-            this.txtMinForSignature.Location = new System.Drawing.Point(431, 45);
+            this.txtMinForSignature.Location = new System.Drawing.Point(575, 55);
+            this.txtMinForSignature.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtMinForSignature.Name = "txtMinForSignature";
             this.txtMinForSignature.ReadOnly = true;
-            this.txtMinForSignature.Size = new System.Drawing.Size(100, 20);
+            this.txtMinForSignature.Size = new System.Drawing.Size(132, 22);
             this.txtMinForSignature.TabIndex = 1;
             // 
             // txtMinForGrade
             // 
-            this.txtMinForGrade.Location = new System.Drawing.Point(583, 45);
+            this.txtMinForGrade.Location = new System.Drawing.Point(777, 55);
+            this.txtMinForGrade.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtMinForGrade.Name = "txtMinForGrade";
             this.txtMinForGrade.ReadOnly = true;
-            this.txtMinForGrade.Size = new System.Drawing.Size(100, 20);
+            this.txtMinForGrade.Size = new System.Drawing.Size(132, 22);
             this.txtMinForGrade.TabIndex = 2;
             // 
             // txtTeacher
             // 
-            this.txtTeacher.Location = new System.Drawing.Point(64, 340);
+            this.txtTeacher.Location = new System.Drawing.Point(85, 418);
+            this.txtTeacher.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtTeacher.Name = "txtTeacher";
             this.txtTeacher.ReadOnly = true;
-            this.txtTeacher.Size = new System.Drawing.Size(100, 20);
+            this.txtTeacher.Size = new System.Drawing.Size(132, 22);
             this.txtTeacher.TabIndex = 3;
             // 
             // txtEvaluationDate
             // 
-            this.txtEvaluationDate.Location = new System.Drawing.Point(179, 341);
+            this.txtEvaluationDate.Location = new System.Drawing.Point(239, 420);
+            this.txtEvaluationDate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtEvaluationDate.Name = "txtEvaluationDate";
             this.txtEvaluationDate.ReadOnly = true;
-            this.txtEvaluationDate.Size = new System.Drawing.Size(100, 20);
+            this.txtEvaluationDate.Size = new System.Drawing.Size(132, 22);
             this.txtEvaluationDate.TabIndex = 4;
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(515, 338);
+            this.btnSave.Location = new System.Drawing.Point(687, 416);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.Size = new System.Drawing.Size(100, 28);
             this.btnSave.TabIndex = 5;
             this.btnSave.Text = "Spremi";
             this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(608, 338);
+            this.btnCancel.Location = new System.Drawing.Point(811, 416);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.Size = new System.Drawing.Size(100, 28);
             this.btnCancel.TabIndex = 6;
             this.btnCancel.Text = "Odustani";
             this.btnCancel.UseVisualStyleBackColor = true;
@@ -104,79 +112,87 @@
             // 
             // numPoints
             // 
-            this.numPoints.Location = new System.Drawing.Point(298, 341);
+            this.numPoints.Location = new System.Drawing.Point(397, 420);
+            this.numPoints.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.numPoints.Name = "numPoints";
-            this.numPoints.Size = new System.Drawing.Size(46, 20);
+            this.numPoints.Size = new System.Drawing.Size(61, 22);
             this.numPoints.TabIndex = 7;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(59, 29);
+            this.label1.Location = new System.Drawing.Point(79, 36);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.Size = new System.Drawing.Size(61, 16);
             this.label1.TabIndex = 8;
             this.label1.Text = "Aktivnost";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(428, 29);
+            this.label2.Location = new System.Drawing.Point(571, 36);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.Size = new System.Drawing.Size(95, 16);
             this.label2.TabIndex = 9;
             this.label2.Text = "Uvjet za potpis";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(580, 29);
+            this.label3.Location = new System.Drawing.Point(773, 36);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.Size = new System.Drawing.Size(98, 16);
             this.label3.TabIndex = 10;
             this.label3.Text = "Uvjet za ocjenu";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(187, 325);
+            this.label4.Location = new System.Drawing.Point(249, 400);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.Size = new System.Drawing.Size(46, 16);
             this.label4.TabIndex = 11;
             this.label4.Text = "Datum";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(61, 322);
+            this.label5.Location = new System.Drawing.Point(81, 396);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(57, 13);
+            this.label5.Size = new System.Drawing.Size(70, 16);
             this.label5.TabIndex = 12;
             this.label5.Text = "Ocjenjivač";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(295, 322);
+            this.label6.Location = new System.Drawing.Point(393, 396);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(40, 13);
+            this.label6.Size = new System.Drawing.Size(50, 16);
             this.label6.TabIndex = 13;
             this.label6.Text = "Bodovi";
             // 
             // txtActivityDescription
             // 
-            this.txtActivityDescription.Location = new System.Drawing.Point(62, 99);
+            this.txtActivityDescription.Location = new System.Drawing.Point(83, 122);
+            this.txtActivityDescription.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtActivityDescription.Multiline = true;
             this.txtActivityDescription.Name = "txtActivityDescription";
             this.txtActivityDescription.ReadOnly = true;
-            this.txtActivityDescription.Size = new System.Drawing.Size(621, 208);
+            this.txtActivityDescription.Size = new System.Drawing.Size(827, 255);
             this.txtActivityDescription.TabIndex = 14;
             // 
             // FrmEvaluation
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.txtActivityDescription);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -192,6 +208,7 @@
             this.Controls.Add(this.txtMinForGrade);
             this.Controls.Add(this.txtMinForSignature);
             this.Controls.Add(this.cboActivities);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FrmEvaluation";
             this.Text = "FrmEvaluation";
             this.Load += new System.EventHandler(this.FrmEvaluation_Load);
--- FrmLogin.cs
+++ FrmLogin.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Evaluation_Manager.Models;
+using Evaluation_Manager.Repositories;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,7 @@ namespace Evaluation_Manager
 {
     public partial class FrmLogin : Form
     {
-        string username = "nastavnik";
-        string password = "test";
-
+        public static Teacher LoggedTeacher { get; set; }
         public FrmLogin()
         {
             InitializeComponent();
@@ -32,7 +32,8 @@ namespace Evaluation_Manager
             }
             else
             {
-                if (txtUsername.Text == username && txtPassword.Text == password)
+                LoggedTeacher = TeacherRepository.GetTeacher(txtUsername.Text);
+                if (LoggedTeacher != null && LoggedTeacher.checkPassword(txtPassword.Text))
                 {
                     FrmStudents frmStudents = new FrmStudents();
                     Hide();
